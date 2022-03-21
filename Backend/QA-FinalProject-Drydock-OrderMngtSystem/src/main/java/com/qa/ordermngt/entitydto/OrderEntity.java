04ac2ea20dd8c83ed896f8c8fc4b9f8c1dc75406
GH <georgeheath.98840@gmail.com>
2022-03-21 12:35:05 +0000

    GH: Pushing populated model and entity classes to the existing skeleton of the project in dev

--- Backend/QA-FinalProject-Drydock-OrderMngtSystem/src/main/java/com/qa/ordermngt/entitydto/OrderEntity.java
+++ Backend/QA-FinalProject-Drydock-OrderMngtSystem/src/main/java/com/qa/ordermngt/entitydto/OrderEntity.java
@@ -1,5 +1,48 @@
 package com.qa.ordermngt.entitydto;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@NoArgsConstructor
 public class OrderEntity {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private long id;
+
+	@Column(nullable = false, length = 80)
+	private String customer;
+	@Column(nullable = false, length = 80)
+	private String vehicleType;
+	@Column(nullable = false)
+	private int displacement;
+	@Column(nullable = false)
+	private boolean military;
+	@Column(nullable = false)
+	private boolean weaponised;
+	@Column(nullable = false)
+	private int resourcesRequired;
+	@Column(nullable = false)
+	private float cost;
+
+	public OrderEntity(String customer, String vehicleType, int displacement, boolean military, boolean weaponised,
+			int resourcesRequired, float cost) {
+		super();
+		this.customer = customer;
+		this.vehicleType = vehicleType;
+		this.displacement = displacement;
+		this.military = military;
+		this.weaponised = weaponised;
+		this.resourcesRequired = resourcesRequired;
+		this.cost = Math.round((displacement * resourcesRequired / 1.5f));
+	}
 
 }
--- Backend/QA-FinalProject-Drydock-OrderMngtSystem/src/main/java/com/qa/ordermngt/model/Order.java
+++ Backend/QA-FinalProject-Drydock-OrderMngtSystem/src/main/java/com/qa/ordermngt/model/Order.java
@@ -1,5 +1,32 @@
 package com.qa.ordermngt.model;
 
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@SuppressWarnings("unused")
 public class Order {
+
+	private long id;
+	private String customer;
+	private String vehicleType;
+	private int displacement;
+	private boolean military;
+	private boolean weaponised;
+	private int resourcesRequired;
+	private float cost;
+
+	public Order(String customer, String vehicleType, int displacement, boolean military, boolean weaponised,
+			int resourcesRequired, float cost) {
+		super();
+		this.customer = customer;
+		this.vehicleType = vehicleType;
+		this.displacement = displacement;
+		this.military = military;
+		this.weaponised = weaponised;
+		this.resourcesRequired = resourcesRequired;
+		this.cost = Math.round((displacement * resourcesRequired / 1.5f));
+	}
 
 }
